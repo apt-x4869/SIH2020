e05333e4de66da061a9de4a9c1c091a0fb22a930
root <root@localhost.localdomain>
2020-06-30 19:45:40 +0530

    error analysis

--- edith/__init__.py
+++ edith/__init__.py
@@ -15,16 +15,7 @@ def default(request, responder):
 
 @app.handle(intent='greet')
 def welcome(request, responder):
-    """
-    When the user starts a conversation, say hi and give some restaurant suggestions to explore.
-    """
-    try:
-        # Get user's name from session information in a request to personalize the greeting.
-        responder.slots['name'] = request.context['name']
-        prefix = 'Hello, {name}. '
-    except KeyError:
-        prefix = 'Hello. '
+    prefix = 'Hello.'
 
-    # Build up the final natural language response and reply to the user.
-    responder.reply( prefix + "Hi, How can I help you?" + " \n-----------------------------------------------"
-                            + str(request) + "\n -----------------------------------------------")
+    print(request)
+    responder.reply( prefix + "Hi, How can I help you?")
