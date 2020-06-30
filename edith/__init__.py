52e1f27a5823465b4d9db78505ac459e3f859e5b
root <root@localhost.localdomain>
2020-06-30 08:08:30 -0400

    template with greet

---
+++ edith/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""This module contains a template MindMeld application"""
+from mindmeld import Application
+
+app = Application(__name__)
+
+__all__ = ['app']
+
+
+@app.handle(default=True)
+def default(request, responder):
+    """This is a default handler."""
+    responder.reply('Hello there!')
+
+
+@app.handle(intent='greet')
+def welcome(request, responder):
+    """
+    When the user starts a conversation, say hi and give some restaurant suggestions to explore.
+    """
+    try:
+        # Get user's name from session information in a request to personalize the greeting.
+        responder.slots['name'] = request.context['name']
+        prefix = 'Hello, {name}. '
+    except KeyError:
+        prefix = 'Hello. '
+
+    # Build up the final natural language response and reply to the user.
+    responder.reply( prefix + "Hi, How can I help you?")
